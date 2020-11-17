c373c9b886af7771a5459a7691e7981afeb6b569
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-17 18:10:42 +0100

    booking user story

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,12 @@
 class BookingsController < ApplicationController
-
   def create
     @booking = Booking.new(booking_params)
+    @offer = Offer.find(params[:offer_id])
     authorize @booking
-    if @booking.save
-      redirect_to booking_path(@booking)
+    @booking.offer = @offer
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to offer_path(@offer)
     else
       render :new
     end
@@ -15,6 +17,8 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @offer = Offer.find(params[:offer_id])
+    authorize @booking
   end
 
   private
@@ -24,6 +28,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    # params.require(:booking).permit(:name, :dose, :photo)
+    params.require(:booking).permit(:starting_time, :ending_time, :number_visitor)
   end
 end
