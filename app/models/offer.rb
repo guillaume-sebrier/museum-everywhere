c7edf025b39b6c8b80d161b449d3dd0c365a67e9
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-18 12:30:08 +0100

    changed seed

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   CATEGORY = ['Unusual collection', 'Design', 'Animals', 'Outdoors', 'Famous People Flat', 'Music', 'Gardens & plants', 'Objects', 'Others']
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_many_attached :photos
 
