dd8f95cc236eca8a0679f4ee4ed6b4f3f9f5a3d0
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-20 11:22:15 +0100

    typed

--- app/javascript/plugins/init_typed.js
+++ app/javascript/plugins/init_typed.js
@@ -2,13 +2,14 @@ import Typed from 'typed.js';
 
 const options = {
   strings: [
-  'Find art in every corner',
-  "Visit your neighbour's seashell collection",
-  "Discover unexpected wonders",
-  "Exchange with crazy enthusiasts",
+  'Find art in <strong>surprising places </strong> <i class="fas fa-paint-brush"> ',
+  "Visit your neighbour's incredible <strong>fish collection</strong> <i class='fas fa-fish'>",
+  "Discover unexpected <strong>wonders</strong> <i class='fas fa-archway'></i>",
+  "Exchange with crazy <strong>enthusiasts</strong> <i class='far fa-laugh-squint'></i>",
   'We can use Typed.js too'],
   typeSpeed: 80,
-  loop: true
+  loop: true,
+  backSpeed: 20,
 };
 
 
