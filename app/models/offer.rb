caf01ee388127b4fcc71d004b928598f9314d1fc
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-20 11:49:21 +0100

    typed2

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,5 +9,6 @@ class Offer < ApplicationRecord
   validates :category, inclusion: { in: CATEGORY }
 
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode
+  #if: :will_save_change_to_address?
 end
